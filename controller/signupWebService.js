6a675dff0604962ab42fabb09bac127996acc94a
sanjaypatidar <sanjaypatidar402@gmail.com>
2019-02-04 12:14:03 +0530

    perameter

--- controller/signupWebService.js
+++ controller/signupWebService.js
@@ -9,19 +9,19 @@ router.post("/",
     check('mobile').isLength({ min: 10 }).withMessage('must be at least 10 chars long'),
     check('city').matches("^[A-Za-z]+$").withMessage("city must be required"),
     // check("adress").matches("^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-,]).{8,}$")
-    check('adress').matches("[A-Za-z0-9-,]+$").withMessage("adress must be required"),
+    check('address').matches("[A-Za-z0-9-,]+$").withMessage("adress must be required"),
     check("state").matches("^[A-Za-z]+$").withMessage("state is must be require"),
     check("lat").isLength({ min: 1 }).withMessage("lat is must be require"),
     check("long").isLength({ min: 1 }).withMessage("lat no. is must be require"),
     check("otp").isLength({ min: 1 }).withMessage("otp no. is must be require"),
     check("IEMI").matches("[0-9]+$").withMessage("IEMI no. is must be require"),
     check("password").matches("^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$").withMessage('must be atleast degit ,specil and alph'),
-    check('divceId').isLength({ min: 1 }).withMessage("diveID must be required")
+    check('deviceId').isLength({ min: 1 }).withMessage("diveID must be required")
     
 
 	,function(req,res){
 
-        
+
 
 const errors = validationResult(req);
    if (!errors.isEmpty()) {
