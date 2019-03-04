b60811fe0b87a845a5faf47e74f3ffe4b6212050
sanjaypatidar <sanjaypatidar402@gmail.com>
2019-03-04 13:49:46 +0530

    4 march

--- controller/subscription.js
+++ controller/subscription.js
@@ -38,7 +38,11 @@ var Mongodb=require("mongodb")
 router.post("/",function(req,res){
   add_cart.findWhere({productid:req.body.productid},{userid:req.body.userid},function(err,result){
     if(result.length>0){
-       
+        add_cart.updatewhere({productid:req.params.productid},{userid:req.params.userid},req.body,function(err,result){
+        add_cart.findWhere({userid=req.body.userid},function(err,result){
+          res.send(result)
+        });
+   });     
     }else{
 
     }
