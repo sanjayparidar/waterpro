e8035c305caac4c1aad963c552f0cc6cf14b1f27
sanjaypatidar <sanjaypatidar402@gmail.com>
2019-03-04 13:58:10 +0530

    4 march

--- controller/subscription.js
+++ controller/subscription.js
@@ -39,7 +39,7 @@ router.post("/",function(req,res){
   add_cart.findWhere({productid:req.body.productid},{userid:req.body.userid},function(err,result){
     if(result.length>0){
         add_cart.updatewhere({productid:req.params.productid},{userid:req.params.userid},req.body,function(err,result){
-        add_cart.findWhere({userid=req.body.userid},function(err,result){
+        add_cart.findWhere({userid:req.body.userid},function(err,result){
           res.send(result)
         });
    });     
