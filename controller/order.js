c9751c8d37f383c89f7387f1dd1cd29cd5105ec3
sanjay <sanjaypatidar402@gmail.com>
2019-03-13 18:15:01 +0530

    13 march

--- controller/order.js
+++ controller/order.js
@@ -1,19 +1,35 @@
 var express=require("express");
 var router=express.Router();
 const Razorpay = require('razorpay');
-var onpayment = require("../model/onpayment");
+var order = require("../model/order");
+var add_cart=require("../model/add_cart");
 
 router.post("/",function(req,res){
-	order.insert(req.body,function(err,res){
-		res.send(result.ops)
-	});
+	add_cart.findWhere({userid:req.body.userid},function(err,result){
+	  order.insert(result,function(err,result){
+		  res.send(result)
+		  add_cart.remove({userid:req.body.userid},function(err,result){
+              
+		  });
+
+	     
+	  });
+   });	
 });
 
-router.get('/',function(req,res){
+router.post('/viewallorder',function(req,res){
 	order.find(function(err,result){
         res.send(result)
 	});
 });
+
+router.post("/vieworder",function(req,res){
+	order.findWhere({userid:req.body.userid},function(err,result){
+		res.send(result)
+	});
+});
+
+
 module.exports=router;
 
   
--- model/order.js
+++ model/order.js
@@ -4,32 +4,26 @@ var config = require("../config/db");
 module.exports.insert=function(obj, cb){
 	connection.init(function(err, client){
 		var db = client.db(config.dbName);
-		db.collection('onpayment').insert(obj, cb);
+		db.collection('order').insert(obj, cb);
 	});
 }
 module.exports.find=function(cb){
 	connection.init(function(err, client){
 		var db = client.db(config.dbName);
-		db.collection('onpayment').find().toArray(cb);
+		db.collection('order').find().toArray(cb);
 	});
 }
 module.exports.findWhere=function(obj,cb){
 	connection.init(function(err,client){
 		var db =client.db(config.dbName);
-		db.collection('onpayment').find(obj).toArray(cb);
+		db.collection('order').find(obj).toArray(cb);
 	})
 }
 
-module.exports.update=function(where, obj, cb){
-	connection.init(function(err, client){
-		var db = client.db(config.dbName);
-		db.collection('onpayment').update(where, {$set : obj}, cb);
-	});
 
-}
 module.exports.remove=function(obj,cb){
 	connection.init(function(err,client){
 		var db=client.db(config.dbName);
-		db.collection('onpayment').remove(obj,cb)
+		db.collection('order').remove(obj,cb)
 	});
 }
