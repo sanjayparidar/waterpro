5467ebe2eb0b7384fc5ac05be9e2b0fad4b6a36b
sanjay <sanjaypatidar402@gmail.com>
2019-04-05 19:01:02 +0530

    5 april

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -3,7 +3,8 @@ var router=express.Router();
 var order=require("../model/order");
 
 router.post("/",function(req,res){
-    order.findWhere({userid:req.body.userid},{paymentid:req.body.paymentid},function(err,result){
+    // {userid:req.body.userid},{paymentid:req.body.paymentid}
+    order.findWhere({ $and: [ {userid:req.body.userid}, {paymentid:req.body.paymentid} ] },function(err,result){
         var category=JSON.parse(result.category)
         var  Quentity=JSON.parse(result.Quentity);
         var array=[ ]
