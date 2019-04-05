5e821ec5aad8297ea21e0c608e9a63e26f1d379e
sanjay <sanjaypatidar402@gmail.com>
2019-04-05 16:39:26 +0530

    1 april

--- controller/order.js
+++ controller/order.js
@@ -33,38 +33,48 @@ var hello='hello'
 var category=JSON.parse(req.body.category);
 var avelabelQuentity=JSON.parse(req.body.avelabelQuentity);
 var Quentity=JSON.parse(req.body.Quentity);
-console.log(category,avelabelQuentity,Quentity)
-var order1={"userid":req.body.userid,"paymentid":req.body.paymentid,"total":req.body.total,"discounttotal":req.body.discounttotal}
+// console.log(category,avelabelQuentity,Quentity)
+// var order1={"userid":req.body.userid,"paymentid":req.body.paymentid,"total":req.body.total,"discounttotal":req.body.discounttotal}
 
- console.log(order1 ,"console 37 ++++++++++++++++++++++++++++_______________")
-add_cart.findWhere({userid:req.body.userid},function(err,result){
-	   console.log("helllo")
-	   if(result.length==0){
-		   var result = [ ]
-           for(var i=0; i<category.length; i++){
-			   result[i].paymentid=req.body.paymentid
-			   result[i].Quentity=req.body.Quentity[i]
-			   result[i].userid=req.body.userid
-			   result[i].productid=req.body.productid[i]
+//  console.log(order1 ,"console 37 ++++++++++++++++++++++++++++_______________")
+// add_cart.findWhere({userid:req.body.userid},function(err,result){
+// 	   console.log("helllo")
+// 	   if(result.length==0){
+// 		   var result = [ ]
+//            for(var i=0; i<category.length; i++){
+// 			   result[i].paymentid=req.body.paymentid
+// 			   result[i].Quentity=req.body.Quentity[i]
+// 			   result[i].userid=req.body.userid
+// 			   result[i].productid=req.body.productid[i]
 			   
-		   }
-	   }
-	   order.insert(order1,function(err,result1){
-                for(let i=0; i<result.length; i++ ){
-					result[i].paymentid=req.body.paymentid
-					result[i].Quentity=req.body.Quentity[i]
-					numberofbottle.updateWhere({category:category[i]},{Quentity:avelabelQuentity[i]-Quentity[i]},function(err,result2){
+// 		   }
+// 	   }
+// 	   order.insert(order1,function(err,result1){
+//                 for(let i=0; i<result.length; i++ ){
+// 					result[i].paymentid=req.body.paymentid
+// 					result[i].Quentity=req.body.Quentity[i]
+// 					numberofbottle.updateWhere({category:category[i]},{Quentity:avelabelQuentity[i]-Quentity[i]},function(err,result2){
 
-					});
-				}
-				orderhistory.insert(result,function(err,result3){
-					add_cart.remove({userid:req.body.userid},function(err,result4){
-						res.send(result1.ops)
+// 					});
+// 				}
+// 				orderhistory.insert(result,function(err,result3){
+// 					add_cart.remove({userid:req.body.userid},function(err,result4){
+// 						res.send(result1.ops)
 
-					});
-				});
-	   });
-   });
+// 					});
+// 				});
+// 	   });
+//    });
+
+      order.insert(req.body,function(err,result){
+		  for(let i=0; i<category.length; i++){
+			numberofbottle.updateWhere({category:category[i]},{Quentity:avelabelQuentity[i]-Quentity[i]},function(err,result2){
+				data={ }
+				data.response="success"
+				res.send(data)
+			});  
+		  }
+	  })
 });
 
 router.post('/viewallorder',function(req,res){
