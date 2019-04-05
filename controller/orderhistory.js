3940cb950c3d7b2284a387c2668f8dce0aa6e9dd
sanjay <sanjaypatidar402@gmail.com>
2019-04-05 18:25:16 +0530

    5 april

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -1,10 +1,19 @@
 var express=require("express");
 var router=express.Router();
-orderhistory=require("../model/orderhistory");
+order=require("../model/order");
 
 router.post("/",function(req,res){
-    orderhistory.findWhere({userid:req.body.userid},{paymentid:req.body.paymentid},function(err,result){
-        res.send(result)
+    order.findWhere({userid:req.body.userid},{paymentid:req.body.paymentid},function(err,result){
+        var category=JSON.parse(result.category)
+        var  Quentity=JSON.parse(result.Quentity);
+        var array=[ ]
+        for( var i=0; i<category.length; i++){
+        array[i].category=category[i]
+        arrya[i].Quentity=Quentity[i]
+        array[i].paymentid=paymentid
+        }
+        res.send(array)
+        // res.send(result)
     });
 });
 
