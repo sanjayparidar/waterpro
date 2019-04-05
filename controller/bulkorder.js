6a01e4470e6a3eb32628100e9be77b09df76649c
sanjay <sanjaypatidar402@gmail.com>
2019-04-05 15:11:55 +0530

    5 april

--- controller/bulkorder.js
+++ controller/bulkorder.js
@@ -3,6 +3,8 @@ var router=express.Router();
 var bulkorder=require("../model/bulkorder");
 router.post('/',function(req,res){
     bulkorder.insert(req.body,function(err,result){
-
+      res.send("success")   
     })
-})
+});
+
+module.exports=router;
