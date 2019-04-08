8252427797d28b3a93c52f44c5cad13c4ec36aaf
sanjay <sanjaypatidar402@gmail.com>
2019-04-08 14:56:08 +0530

    5 april

--- controller/order.js
+++ controller/order.js
@@ -71,6 +71,7 @@ var Quentity=JSON.parse(req.body.Quentity);
 		 console.log('hello')
 		 req.body.date = ("0"+new Date().getDate()).slice(-2)+'-'+("0"+(new Date().getMonth()+1)).slice(-2)+'-'+("0"+new Date().getFullYear()).slice(-4);
 		 req.body.time=("0"+new Date().getHours()).slice(-2)+':'+("0"+new Date().getMinutes()).slice(-2);
+		 console.log(req.body.time,"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
       order.insert(req.body,function(err,result){
 		  for(let i=0; i<category.length; i++){
 			numberofbottle.updateWhere({category:category[i]},{Quentity:avelabelQuentity[i]-Quentity[i]},function(err,result2){
