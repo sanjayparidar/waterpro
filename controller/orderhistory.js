606bbee67c968570e3acfc35ec0b46ec6fc450c9
sanjay <sanjaypatidar402@gmail.com>
2019-04-08 12:38:53 +0530

    5 april

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -5,7 +5,7 @@ var Mongo=require("mongodb");
 
 router.post("/",function(req,res){
     // {userid:req.body.userid},{paymentid:req.body.paymentid}
-    order.findWhere({ $and: [ {userid:req.body.userid}, { _id : Mongo.ObjectId(id)} ] },function(err,result){
+    order.findWhere({ _id : Mongo.ObjectId(req.body.id)},function(err,result){
         var category=JSON.parse(result[0].category)
         var  Quentity=JSON.parse(result[0].Quentity);
         var array=[ ]
